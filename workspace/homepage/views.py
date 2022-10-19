f4b26dd0d23684e78afaaecefd58f8f0a95d8077
sophryu99 <sehwa_ryu@berkeley.edu>
2022-10-19 15:42:34 -0700

    add: configure url routing for the landing page and the main page

--- workspace/homepage/views.py
+++ workspace/homepage/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+import sys
+import os
+import json
+import re
 from django.http import HttpResponse
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the homepage index.")
+
+
+# Landing Page
 def index(request):
-    return HttpResponse("Hello, world. You're at the homepage index.")
+    if request.method == 'GET':
+        return render(request, 'homepage/index.html',
+                      {
+                      }
+            )
+
+# Main Page
+def getMainPage(request):
+    if request.method == 'GET':
+        return render(request, 'homepage/main.html',
+                      {
+                      }
+            )
