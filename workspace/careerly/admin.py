86c659ad6c1a6ac62c174dc21f9c56b904cf0b7e
sophryu99 <sehwa_ryu@berkeley.edu>
2022-11-07 19:44:47 -0800

    add: create admin user

--- workspace/careerly/admin.py
+++ workspace/careerly/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Careerly
+
+class CareerlyAdmin(admin.ModelAdmin):
+    list_display = ('jobtitle', 'company')
+
 # Register your models here.
+
+admin.site.register(Careerly, CareerlyAdmin)
--- workspace/careerly/models.py
+++ workspace/careerly/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Careerly(models.Model):
+    jobtitle = models.CharField(max_length=120)
+    company = models.TextField()
+    # completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.jobtitle
